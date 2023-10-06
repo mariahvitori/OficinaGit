9da8475103e3f6754aa53c0e7411bc0d5df4165a
Mariah <mariahvitoriabastistacardoso@gmail.com>
2023-10-06 09:39:34 -0400

    segundo commit

--- Oficina Git/Program.cs
+++ Oficina Git/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+class progran
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("\ndigite um numero:");
+        double num = Convert.ToDouble(Console.ReadLine()); ;
+        Console.WriteLine("digite outro numero:");
+         double num2= Convert.ToDouble(Console.ReadLine());
+
+        double resultado = num * num2;
+        Console.WriteLine("\nresultado da sua multiplicação é"+ resultado);
+        if (resultado == 0) { Console.WriteLine("numero invalidoo"); }
+        if (resultado == 1) { Console.WriteLine("numero invalidoo"); }
+        if (resultado == 2) { Console.WriteLine("numero invalidoo"); }
+
+
+
+
+        Console.ReadKey();
+    }
+
+
+
+}
